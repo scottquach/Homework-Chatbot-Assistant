b89302fdd9b22f2163503513c4d8e03245a0056b
scott quach <scottqglobal@gmail.com>
2017-10-04 17:56:55 -0700

    Restart alarm manager on device reboot

---
+++ app/src/main/java/com/scottquach/homeworkchatbotassistant/RebootReceiver.java
@@ -0,0 +1,18 @@
+package com.scottquach.homeworkchatbotassistant;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Scott Quach on 10/4/2017.
+ * Re initialize class end reminder alarms that were destroyed on device reboot
+ */
+
+public class RebootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotifyClassEndManager manager = new NotifyClassEndManager(context);
+        manager.startManaging();
+    }
+}
