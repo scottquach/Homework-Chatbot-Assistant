fb40904160d58bf90a442182ac1c5df6c969bc92
Joe Villegas <joevillegasisawesome@gmail.com>
2017-11-03 21:21:39 -0700

    Revert "fixed crashes for Notifications relating string usages"
    This reverts commit df2ab0c1c521384b1e0657853e4b59570a8a3955.

--- app/src/main/java/com/scottquach/homeworkchatbotassistant/utils/StringUtils.kt
+++ app/src/main/java/com/scottquach/homeworkchatbotassistant/utils/StringUtils.kt
@@ -28,7 +28,7 @@ class StringUtils {
                     2 -> displayString += "M"
                     3 -> displayString += "T"
                     4 -> displayString += "W"
-                    5 -> displayString += "Th"
+                    5 -> displayString += "T"
                     6 -> displayString += "F"
                     7 -> displayString += "S"
                 }
